1162a07dda5ef96a4ceb79ed045a6803d0a48b4b
Ron MacBook Air <laironald@gmail.com>
2013-05-10 15:52:17 -0500

    weakly attempt oauth code storage

--- quickstart.py
+++ quickstart.py
@@ -11,6 +11,11 @@ from oauth2client.client import OAuth2WebServerFlow
 # Copy your credentials from the APIs Console
 CLIENT_ID = '225117007438-c93selkie7am9u437rek3ih1fikd51r2.apps.googleusercontent.com'
 CLIENT_SECRET = 'Qih7X5Ssa3Wu0Tcfw5p5tpen'
+#oauth_code = '4/ZxKaQ29rHgLBxwgVDgZCdIMIEVwB.MmgIWm-PIa8QuJJVnL49Cc_Euj4gfQI'
+oauth_code = ""
+
+# Path to the file to upload
+FILENAME = 'document.txt'
 
 # Check https://developers.google.com/drive/scopes for all available scopes
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
@@ -18,15 +23,14 @@ OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 # Redirect URI for installed apps
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-# Path to the file to upload
-FILENAME = 'document.txt'
-
-# Run through the OAuth flow and retrieve credentials
 flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-authorize_url = flow.step1_get_authorize_url()
-print 'Go to the following link in your browser: ' + authorize_url
-code = raw_input('Enter verification code: ').strip()
-credentials = flow.step2_exchange(code)
+if not oauth_code:
+  # Run through the OAuth flow and retrieve credentials
+  authorize_url = flow.step1_get_authorize_url()
+  print 'Go to the following link in your browser: ' + authorize_url
+  oauth_code = raw_input('Enter verification code: ').strip()
+
+credentials = flow.step2_exchange(oauth_code)
 
 # Create an httplib2.Http object and authorize it with our credentials
 http = httplib2.Http()
