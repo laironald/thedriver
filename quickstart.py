5d99c8ec0881f32980968094418d0998f1a33c9e
Ron MacBook Air <laironald@gmail.com>
2013-05-11 11:41:27 -0500

    added pickling the credentials

--- quickstart.py
+++ quickstart.py
@@ -2,6 +2,8 @@
 
 import httplib2
 import pprint
+import os
+import pickle
 
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
@@ -11,8 +13,6 @@ from oauth2client.client import OAuth2WebServerFlow
 # Copy your credentials from the APIs Console
 CLIENT_ID = '225117007438-c93selkie7am9u437rek3ih1fikd51r2.apps.googleusercontent.com'
 CLIENT_SECRET = 'Qih7X5Ssa3Wu0Tcfw5p5tpen'
-#oauth_code = '4/ZxKaQ29rHgLBxwgVDgZCdIMIEVwB.MmgIWm-PIa8QuJJVnL49Cc_Euj4gfQI'
-oauth_code = ""
 
 # Path to the file to upload
 FILENAME = 'document.txt'
@@ -23,14 +23,23 @@ OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 # Redirect URI for installed apps
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-if not oauth_code:
+# PICKLE ------------------
+#
+if os.path.exists("pickle/oauth_code.pick"):
+  credentials = None
+else:
+  credentials = pickle.load(open("pickle/oauth_code.pick", "rb"))
+#
+# =========================
+
+if not credentials:
+  flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
   # Run through the OAuth flow and retrieve credentials
   authorize_url = flow.step1_get_authorize_url()
   print 'Go to the following link in your browser: ' + authorize_url
   oauth_code = raw_input('Enter verification code: ').strip()
-
-credentials = flow.step2_exchange(oauth_code)
+  credentials = flow.step2_exchange(oauth_code)
+  pickle.dump(credentials, open("pickle/oauth_code.pick", "wb"))
 
 # Create an httplib2.Http object and authorize it with our credentials
 http = httplib2.Http()
