5077289cc67f27f73f0c769d81c715377e2156f6
liwenz <liwenz@liwenz-T430-Ubuntu.(none)>
2013-06-07 10:50:27 -0500

    added upload_html function for the uploader

--- thedriver/upload.py
+++ thedriver/upload.py
@@ -5,48 +5,88 @@ import pickle
 import httplib2
 import re
 import urllib2
+
+from apiclient.http import MediaFileUpload
 from apiclient.discovery import build
 
 class uploader():
     driver = None
-    folderLink = None
+    uploadFolder = None
 
-    def find_public_folder(self, folder_name):
+    def find_publish_folder(self, folder_name):
+        '''Check if there exists any public folder whose name contains folder_name.
+	Args:
+	folder_name: keywords of public folder's name.
+
+	Returns:
+	If folders exist, returns a list of Drive file instance of these folders.
+	Otherwise, returns [].
+	'''
         files = self.driver.files(title=folder_name)
         files = filter(
-          lambda x: x['mimeType'] == 'application/vnd.google-apps.folder'
-            and (not 'explicitlyTrashed' in x or not x['explicitlyTrashed']) 
-	    and 'webViewLink' in x,
+          lambda x: x["mimeType"] == "application/vnd.google-apps.folder"
+            and (not "explicitlyTrashed" in x or not x["explicitlyTrashed"]) 
+	    and "webViewLink" in x,
           files)
         return files
 
-    def create_public_folder(self, folder_name="publish_folder"):
+    def create_publish_folder(self, folder_name="publish_folder"):
+        '''Create a public folder for uploading files to be published.
+        Args:
+	folder_name: name of the public folder.
+
+	Returns:
+        Driver files instance for the created folder.  
+	The folder has mimeType 'application/vnd.google-apps.folder'.
+	'''
         import thedriver
         if self.driver.service==None:
             self.driver.build()
         body = { 
-		'title': folder_name, 
-		'mimeType': 'application/vnd.google-apps.folder'
+		"title": folder_name, 
+		"mimeType": "application/vnd.google-apps.folder"
 	       }
         file = service.files().insert(body=body).execute()
         permission = {
-                      'value': '',
-                      'type': 'anyone',
-                      'role': 'reader'
+                      "value": "",
+                      "type": "anyone",
+                      "role": "reader"
                      }
         
         service.permissions().insert(
-        fileId=file['id'], body=permission).execute()
+        fileId=file["id"], body=permission).execute()
         return file
         
+    def upload_html(self, filename):
+        '''Upload a local html file to publish_folder.
+	Args:
+	filename: path to the file to upload.
+       
+        Returns:
+	Drive File instance for the uploaded file.
+	'''
+        media_body = MediaFileUpload(filename, mimetype="text/html", resumable=True)
+	body = {
+           "title": filename,
+           "description": "a html file uploaded by thedriver",
+	   "mimeType": "text/html",
+	   "parents": [{
+		   "kind":"drive#fileLink",
+		   "id":self.uploadFolder["id"]
+		   }]
+	   }
+	file = self.driver.service.files().insert(body=body, media_body=media_body).execute()
+	print self.uploadFolder["webViewLink"] + filename
+	return file 
+
     def __init__(self, driver=None, folder_name="publish_folder"):
         import thedriver
         if driver == None:
             self.driver = thedriver.go()
 	    self.driver.build()
-        folders = self.find_public_folder(folder_name)
+        folders = self.find_publish_folder(folder_name)
 	if len(folders) == 0:
-		create_public_folder(folder_name)
-		folders = self.find_public_folder(folder_name)
-        self.folderLink = folders[0]['webViewLink']
+		create_publish_folder(folder_name)
+		folders = self.find_publish_folder(folder_name)
+        self.uploadFolder = folders[0]
 
