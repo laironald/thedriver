b2d6ce5eadd3515098375cd200323f28bbbf8966
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-06-13 10:28:06 -0500

    added bootstrap but that's basically it

--- flask/app.py
+++ flask/app.py
@@ -1,24 +1,20 @@
 import sys
 sys.path.append("..")
+
 import thedriver
 import thedriver.download as drived
-
 from flask import Flask, render_template
 from flask.ext.assets import Environment, Bundle
-from hamlish_jinja import HamlishTagExtension
-
+from hamlish_jinja import HamlishExtension
 
 drive = thedriver.go()
 app = Flask(__name__)
 app.debug = True
 
-# add haml
-app.jinja_env.add_extension(HamlishTagExtension)
-
-# compile assets
+# add haml and compile assets
+app.jinja_env.add_extension(HamlishExtension)
 assets = Environment(app)
 assets.url = app.static_url_path
-
 sass = Bundle('css/base.css.sass', filters='sass', output='all.css')
 assets.register('css_all', sass)
 
