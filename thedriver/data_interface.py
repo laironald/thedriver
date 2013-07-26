6006727e5e26e4d774cd16229b5f575de8844cda
None <None>
2013-07-26 16:01:44 -0500

    adding data_interface and ghost_db

---
+++ thedriver/data_interface.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+import httplib2
+import re
+import urllib2
+
+from apiclient.http import MediaFileUpload
+from apiclient.discovery import build
+
+from apiclient import errors
+
+def list_ghost_docs( user_id ):
+        pass
+
+def list_google_docs( user_id, if_hide_ghost_doc=True ):
+        # look up user's google id from d/b.
+        # look up user's auth.
+        # list docs.
+        pass
+
+def list_recent_docs( user_id ):
+        pass
+
+
