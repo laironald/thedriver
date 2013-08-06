748648ea10070801900e1af65414002f5469ec53
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-06 09:11:48 -0700

    renamed marketing to index

--- flask/app.py
+++ flask/app.py
@@ -7,7 +7,6 @@ sys.path.append("..")
 
 import data_interface as di
 from flask import Flask, render_template
-from flask import url_for
 from flask import send_from_directory
 import hamlish_jinja
 import json
@@ -15,7 +14,7 @@ import json
 app = Flask(__name__)
 app.debug = True
 
-app.jinja_env.add_extension('hamlish_jinja.HamlishExtension')
+app.jinja_env.add_extension(hamlish_jinja.HamlishExtension)
 app.jinja_env.hamlish_enable_div_shortcut = True
 
 
@@ -25,53 +24,35 @@ app.jinja_env.hamlish_enable_div_shortcut = True
 @app.route('/')
 def index():
     return render_template(
-        'marketing.html',
+        'index.html',
         title='GhostDocs (>")>',
         googletitle="Testing",
         googledoc="1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4")
 
 
-@app.route('/base')
-def base():
-    return render_template('index.html')
-
-
-# --- STATIC FIXTURES ---
-# added these to help serve up the static files
-
-@app.route('/css/<path:filename>')
-def send_css(filename):
-    return send_from_directory('static/css', filename)
-
-
-@app.route('/js/<path:filename>')
-def send_js(filename):
-    return send_from_directory('static/js', filename)
-
-
-@app.route('/img/<path:filename>')
-def send_img(filename):
-    return send_from_directory('static/img', filename)
-
-# -----------------------
+# --- LOGIN / OUT ---
 
 
 @app.route('/in/<username>/<dochandle>')
 def render_base(username, dochandle):
     doc = di.load_doc(username, dochandle)
     if not doc:
-        return "Weird stuff yo!"
+        return render_template("404.html")
     else:
-        return render_template('marketing.html', doc=doc, user=doc.user)
+        return render_template('index.html', doc=doc, user=doc.user)
 
 
 @app.route('/out/<username>/<dochandle>')
 def render_publish(username, dochandle):
     doc = di.load_doc(username, dochandle)
     if not doc:
-        return "Weird stuff yo!"
+        return render_template("404.html")
     else:
         return render_template('powered.html', doc=doc, user=doc.user)
+
+
+# --- IFRAME SUPPORT ---
+
 
 @app.route('/view/<doc_id>')
 def render_view(doc_id):
@@ -93,6 +74,25 @@ def publish_doc(doc_id):
     status["status"] = "success"
     return json.dumps(status)
 
+
+# --- STATIC FIXTURES ---
+# added these to help serve up the static files
+
+@app.route('/css/<path:filename>')
+def send_css(filename):
+    return send_from_directory('static/css', filename)
+
+
+@app.route('/js/<path:filename>')
+def send_js(filename):
+    return send_from_directory('static/js', filename)
+
+
+@app.route('/img/<path:filename>')
+def send_img(filename):
+    return send_from_directory('static/img', filename)
+
+# -----------------------
 
 if __name__ == '__main__':
     app.run()
