49e1c9df58f94b36de5c5958cef7917f2dc16ce1
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-25 11:02:15 -0400

    added column descriptions

--- data_interface/schema.py
+++ data_interface/schema.py
@@ -7,6 +7,12 @@ from sqlalchemy import func
 
 ''' +++++++++++++++++++++++++++++++++++
 Define Tables:
+ * Likely +analytic table at some point?
+   (or should this be all managed by )
+
+ These fields are pesistent in every database:
+ * time_created: the time the entry was created
+ * time_updated: the time that the entry was updated
 +++++++++++++++++++++++++++++++++++ '''
 
 Base = declarative_base()
@@ -14,6 +20,13 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
+    """
+    name: the fullname of the user
+    handle: the hanle of the user (ie. @liwen)
+    google_account: ?
+    oauth_code: <is this sufficient for auth?>
+    email: the email address of the user
+    """
 
     id = Column(Integer, primary_key=True)
     name = Column(String(45), unique=True)
@@ -29,6 +42,25 @@ class User(Base):
 
 
 class Document(Base):
+    """
+    name: the title of the document
+    googledoc_id: google document identifier
+    password: a special password for the document (if protected)
+    handle: the handle of a document (ie. @testing)
+    is_protected: specifies whether the doc is protected
+    is_published: ??
+    alternateLink: url for google iframe
+    htmlLink: document exported as html
+    html: post published generated html
+    share_status
+        0 = public
+        1 = unlisted
+        2 = private
+    time_opened: last time the owner opened the document
+
+    note: we are currently assuming
+        user>document, n>1 relationship
+    """
     __tablename__ = 'document'
 
     id = Column(Integer, primary_key=True)
@@ -41,8 +73,10 @@ class Document(Base):
     alternateLink = Column(String(256))
     htmlLink = Column(String(256))
     html = Column(Text)  # compiled html
+    share_status = Column(Integer, default=0)
     time_created = Column(DateTime, default=func.now())
     time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
+    time_opened = Column(DateTime)
 
     user_id = Column(Integer, ForeignKey('user.id'))
     folder_id = Column(Integer, ForeignKey('folder.id'))
@@ -50,6 +84,10 @@ class Document(Base):
 
 class Folder(Base):
     __tablename__ = 'folder'
+    """
+    name: ??? name of the folder name?
+    is_root: ???
+    """
 
     id = Column(Integer, primary_key=True)
     name = Column(String(45))
