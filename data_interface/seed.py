537bf6c09adbaf706b4cc38b109e75f0841fa5df
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-25 23:20:09 -0400

    added a new doc to seed

--- data_interface/seed.py
+++ data_interface/seed.py
@@ -4,16 +4,25 @@ connector = GhostDBConnector()
 connector.CreateDB()
 user = User(**config.get("drive"))
 
-doc = {
+docs = [{
     "name": "Testing File",
     "googledoc_id": "1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4",
     "handle": "testing",
     "htmlLink": "https://docs.google.com/feeds/download/documents/export/Export?id=1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4&exportFormat=html",
     "alternateLink": "https://docs.google.com/document/d/1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4/edit?usp=drivesdk",
     "html": "".join([x for x in open("temp/ron.html", "rb")])
-}
-doc = Document(**doc)
-user.document.append(doc)
+}, {
+    "name": "Testing File2",
+    "googledoc_id": "1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4",
+    "handle": "testing2",
+    "htmlLink": "https://docs.google.com/feeds/download/documents/export/Export?id=1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4&exportFormat=html",
+    "alternateLink": "https://docs.google.com/document/d/1TE0ouM01lsPvot5aZRDv9D7-xC-kLJ0dg3S_zPdWrO4/edit?usp=drivesdk",
+    "html": "".join([x for x in open("temp/ron.html", "rb")])
+}]
 
-connector.session().merge(user)
+for doc in docs:
+    doc = Document(**doc)
+    user.document.append(doc)
+    connector.session().merge(user)
+
 connector.session().commit()
