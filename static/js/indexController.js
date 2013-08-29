750e65f4afe91aeaba854090005bc5943c7ac72c
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-29 16:02:34 -0400

    adding additional options for settings

--- static/js/indexController.js
+++ static/js/indexController.js
@@ -1,14 +1,14 @@
 var myApp = angular.module('myApp', []);
 myApp.factory('Data', function() {
-    return {"action": 0};
+    return {};
 });
 
 
-function PreviewCtrl($scope, Data) {
+function PreviewModalCtrl($scope, Data) {
     $scope.data = Data;
-    console.log($scope.data);
     $scope.preview = function(data_url) {
         $scope.data.url = "/preview/" + data_url;
+        $scope.data.url = "/view/" + data_url;
         $scope.data.action = 0;
         analytics.track('Change preview option',
             {url: $scope.data.url});
@@ -21,13 +21,28 @@ function PreviewCtrl($scope, Data) {
             {url: $scope.data.url});
     };
 }
+function SettingModalCtrl($scope, $window, $http, Data) {
+    $scope.data = Data;
+    $scope.save = function() {
+        data = {"ron": "ron"};
+        $http.put("/action/settings", data).success(function() {
+            $window.location.href = "/out/ghostie/testing";
+        });
+    };
+}
 
-function MenuClickCtrl($scope, Data) {
+function MenuClickCtrl($scope, $http, Data) {
     $scope.data = Data;
     $scope.preview = function(data_url) {
         $scope.data.url = "/preview/" + data_url;
+        $scope.data.url = "/view/" + data_url;
         $scope.data.action = 0;
         analytics.track('Previewed document',
             {url: $scope.data.url});
+    };
+    $scope.settings = function() {
+        $http.get("/action/settings").success(function(data) {
+            $scope.data.settings = data;
+        });
     };
 }
