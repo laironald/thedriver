13de58ab99404c628115deaa9a200cfd39206f02
Ron Lai (MacBook AIR) <laironald@gmail.com>
2013-08-29 22:36:30 -0500

    refactored to make doc_id less redundant

--- static/js/indexController.js
+++ static/js/indexController.js
@@ -6,15 +6,14 @@ myApp.factory('Data', function() {
 
 function PreviewModalCtrl($scope, Data) {
     $scope.data = Data;
-    $scope.preview = function(data_url) {
-        $scope.data.url = "/preview/" + data_url;
+    $scope.preview = function() {
+        $scope.data.url = "/preview/" + $scope.data.doc_id;
         $scope.data.action = 0;
         analytics.track('Change preview option',
             {url: $scope.data.url});
     };
-
-    $scope.publish = function(data_url) {
-        $scope.data.url = "/view/" + data_url;
+    $scope.publish = function() {
+        $scope.data.url = "/view/" + $scope.data.doc_id;
         $scope.data.action = 1;
         analytics.track('Change preview option',
             {url: $scope.data.url});
@@ -24,24 +23,27 @@ function SettingModalCtrl($scope, $window, $http, Data) {
     $scope.data = Data;
     $scope.save = function() {
         data = {"ron": "ron"};
-        $http.post("/action/post/settings", data).success(function() {
+        url = "/action/post/settings/" + $scope.data.doc_id;
+        $http.post(url, data).success(function() {
             // $window.location.href = "/in/ghostie/testing";
         });
         analytics.track('Saved settings',
             {url: $scope.data.settings.handle});
     };
 }
-
 function MenuClickCtrl($scope, $http, Data) {
     $scope.data = Data;
-    $scope.preview = function(data_url) {
-        $scope.data.url = "/preview/" + data_url;
+    $scope.preview = function(doc_id) {
+        $scope.data.doc_id = doc_id;
+        $scope.data.url = "/preview/" + $scope.data.doc_id;
         $scope.data.action = 0;
         analytics.track('Previewed document',
             {url: $scope.data.url});
     };
-    $scope.settings = function() {
-        $http.get("/action/get/settings").success(function(data) {
+    $scope.settings = function(doc_id) {
+        $scope.data.doc_id = doc_id;
+        url = "/action/get/settings/" + $scope.data.doc_id;
+        $http.get(url).success(function(data) {
             $scope.data.settings = data;
         });
     };
